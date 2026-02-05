b10b3aaa3316f6ba05315d3d0c07fe8f7053fd4f
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2026-02-05 22:41:41 +0100

    Add pkg/json

---
+++ pkg/json/json.go
@@ -0,0 +1,39 @@
+package json //nolint:revive
+
+import (
+	"os"
+
+	libjson "encoding/json"
+)
+
+func Marshal[T any](v T) ([]byte, error) {
+	return libjson.Marshal(v)
+}
+
+func Unmarshal[T any](data []byte) (T, error) {
+	var v T
+
+	err := libjson.Unmarshal(data, &v)
+
+	return v, err
+}
+
+func MarshalToFile[T any](v T, filename string) error {
+	data, err := Marshal(v)
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(filename, data, 0600)
+}
+
+func UnmarshalFromFile[T any](filename string) (T, error) {
+	data, err := os.ReadFile(filename)
+	if err != nil {
+		var v T
+
+		return v, err
+	}
+
+	return Unmarshal[T](data)
+}
--- pkg/s3actions/actions.go
+++ pkg/s3actions/actions.go
@@ -1,4 +1,4 @@
-package actions
+package s3actions
 
 type Action string
 
