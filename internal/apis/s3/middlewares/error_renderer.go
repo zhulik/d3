282a7561b0fb501a0aad9762f057626f1f060c76
Gleb Sinyavskiy <zhulik.gleb@gmail.com>
2026-02-09 01:06:11 +0100

    Get rid of 500s: correctly map errors to statuses

--- internal/apis/s3/middlewares/error_renderer.go
+++ internal/apis/s3/middlewares/error_renderer.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/labstack/echo/v5"
 	"github.com/zhulik/d3/internal/core"
+	"github.com/zhulik/d3/pkg/iampol"
 )
 
 func ErrorRenderer() echo.MiddlewareFunc {
@@ -15,14 +16,21 @@ func ErrorRenderer() echo.MiddlewareFunc {
 			switch {
 			case errors.Is(err, core.ErrBucketNotFound):
 				return echo.NewHTTPError(http.StatusNotFound, err.Error())
-			case errors.Is(err, core.ErrObjectNotFound):
+			case errors.Is(err, core.ErrObjectNotFound) ||
+				errors.Is(err, core.ErrPolicyNotFound) ||
+				errors.Is(err, core.ErrUserNotFound):
 				return echo.NewHTTPError(http.StatusNotFound, err.Error())
-			case errors.Is(err, core.ErrBucketAlreadyExists) || errors.Is(err, core.ErrObjectAlreadyExists):
+			case errors.Is(err, core.ErrBucketAlreadyExists) ||
+				errors.Is(err, core.ErrObjectAlreadyExists) ||
+				errors.Is(err, core.ErrPolicyAlreadyExists) ||
+				errors.Is(err, core.ErrUserAlreadyExists):
 				return echo.NewHTTPError(http.StatusConflict, err.Error())
 			case errors.Is(err, core.ErrBucketNotEmpty):
 				return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 			case errors.Is(err, core.ErrUnauthorized):
 				return echo.NewHTTPError(http.StatusUnauthorized, err.Error())
+			case errors.Is(err, iampol.ErrInvalidPolicy):
+				return echo.NewHTTPError(http.StatusBadRequest, err.Error())
 			case err == nil:
 				return nil
 			default:
